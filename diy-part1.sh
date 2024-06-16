cbf31b660237d634f3c3c2b35007e7b2fcf55188
Akimio521 <akimio2333@gmail.com>
2024-06-16 20:32:47 +0800

    增加nas-packages-luci源

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,5 +19,6 @@ echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.d
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 echo 'src-git gngpp https://github.com/gngpp/luci-theme-design.git' >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
