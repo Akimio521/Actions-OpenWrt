6170cea1205820662e939eef01566d541bdf24db
Akimio521 <akimio2333@gmail.com>
2024-06-16 20:21:06 +0800

    增加luci-theme-design源

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,6 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+echo 'src-git gngpp https://github.com/gngpp/luci-theme-design.git' >> feeds.conf.default
+
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
