293f4dfea3d443ff70a2aaad4d7f7cd94ca4f4e7
Akimio521 <akimio2333@gmail.com>
2024-06-16 20:28:01 +0800

    增加istore源

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,6 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >> feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 echo 'src-git gngpp https://github.com/gngpp/luci-theme-design.git' >> feeds.conf.default
+echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
